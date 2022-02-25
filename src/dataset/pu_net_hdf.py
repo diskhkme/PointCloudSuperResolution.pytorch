fix forward cd_loss calculation, remove init layer

src/dataset/pu_net_hdf.py
@@ -58,8 +58,11 @@ class PUNetDataset(data.Dataset):
         return name_dict
 
     def __getitem__(self, idx):
-        choice = np.random.choice(self.input_data.shape[1], self.npoints, replace=True)
-        batch_input_data = np.expand_dims(self.input_data[idx, choice, :].copy(),0)
+        # choice = np.random.choice(self.input_data.shape[1], self.npoints, replace=True)
+        # batch_input_data = np.expand_dims(self.input_data[idx, choice, :].copy(),0)
+        point_idx = self.nonuniform_sampling(self.input_data.shape[1], self.npoints)
+        batch_input_data = np.expand_dims(self.input_data[idx,point_idx,:].copy(),0)
+
         batch_label_data = self.name_dict[self.get_name_from_name_path(self.names[idx])]
         batch_data_gt = np.expand_dims(self.gt[idx,:,:].copy(),0)
         radius = self.radius[idx]
@@ -89,6 +92,16 @@ class PUNetDataset(data.Dataset):
 
     # Data augmentation is directly copied from official implementation
     # https://github.com/wuhuikai/PointCloudSuperResolution/blob/master/code/data_provider.py
+
+    def nonuniform_sampling(self, num=4096, sample_num=1024):
+        sample = set()
+        loc = np.random.rand() * 0.8 + 0.1
+        while len(sample) < sample_num:
+            a = int(np.random.normal(loc=loc, scale=0.3) * num)
+            if a < 0 or a >= num:
+                continue
+            sample.add(a)
+        return list(sample)
 
     def rotate_point_cloud_and_gt(self, batch_data, batch_gt=None):
         """ Randomly rotate the point clouds to augument the dataset
@@ -199,6 +212,7 @@ class PUNetDataset(data.Dataset):
 
     def __len__(self):
         return len(self.input_data)
+        # return 30
 
 if __name__ == '__main__':
     path = '../../data/Patches_noHole_and_collected.h5'

src/model/grouping_util.py
@@ -13,8 +13,11 @@ def pairwise_dist(gt, pred):
     r_xyz1 = torch.sum(gt * gt, dim=2, keepdim=True)  # (B,N,1)
     r_xyz2 = torch.sum(pred * pred, dim=2, keepdim=True)  # (B,M,1)
     mul = torch.matmul(pred, gt.permute(0, 2, 1))         # (B,M,N) (matmul (b,m,1)x(b,1,n)
-    dist, _ = torch.min(r_xyz2 - 2 * mul + r_xyz1.permute(0,2,1),dim=2)       # (B,M)
+    dist, _ = torch.min(r_xyz2 - 2 * mul + r_xyz1.permute(0,2,1),dim=1)       # (B,M)
     return dist # using squared dist, based on original impl
+
+    # dist, _ = knn_point(1, gt, pred)
+    # return dist
 
 def knn_point(k, xyz1, xyz2):
     b1, d1, n1 = xyz1.shape
@@ -26,7 +29,7 @@ def knn_point(k, xyz1, xyz2):
     val, idx = torch.topk(-dist, k=k, dim=2)
     idx = idx.transpose(1, 2).contiguous()
 
-    return torch.sqrt(-val), idx  # TODO: remove sqrt
+    return -val, idx  # TODO: remove sqrt
 
 def group_point(x, idx):
     k = idx.size(1)
@@ -124,10 +127,10 @@ if __name__ == '__main__':
     # ind = knn_point(8,x,x)
 
 
-    gt = torch.rand((1,3,2))
-    pred = torch.rand((1,3,3))
-    val = pairwise_dist(gt, pred)
+    gt = torch.rand(20,3,4096)
+    pred = torch.rand(20, 3, 1024)
+    val = pairwise_dist(gt,pred)
 
-    print(val.shape)
+    print(val.item())
 
 